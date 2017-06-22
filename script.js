aff0173e6800877f4754a099a6d2ee7b13b43e84
Stephanie Andrews <seandrews11@gmail.com>
2017-06-22 10:30:51 -0600

    User sees a month calendar view

--- script.js
+++ script.js
@@ -20,14 +20,70 @@ const defaultEvent = {
 
 const calEvents = [defaultEvent];
 
-
 const Calendar = function (day, month, year, calEvents) {
   this.day = (isNaN(day) || day == null) ? calCurrentDate.getDay() : day;
   this.month = (isNaN(month) || month == null) ? calCurrentDate.getMonth() : month;
   this.year  = (isNaN(year) || year == null) ? calCurrentDate.getFullYear() : year;
   this.calEvents = calEvents;
+  this.html = '';
 }
 
-const calendar = new Calendar(null, null, null, calEvents);
+Calendar.prototype.generateHTML = function(){
+    var firstDay = new Date(this.year, this.month, 1);
+    var startingDay = firstDay.getDay();
 
-console.log(calendar)
+    var monthLength = calDaysInMonth[this.month];
+
+    if (this.month == 1) { // February only!
+      if((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0){
+        monthLength = 29;
+      }
+    }
+
+    var monthName = calMonths[this.month]
+    var html = '<table class="calendar-table">';
+    html += '<tr><th colspan="7">';
+    html +=  monthName + "&nbsp;" + this.year;
+    html += '</th></tr>';
+    html += '<tr class="calendar-header">';
+    for(var i = 0; i <= 6; i++ ){
+      html += '<td class="calendar-header-day">';
+      html += calDays[i];
+      html += '</td>';
+    }
+    html += '</tr><tr>';
+
+    // fill in the days
+    var day = 1;
+    // this loop is for is weeks (rows)
+    for (var i = 0; i < 9; i++) {
+      // this loop is for weekdays (cells)
+      for (var j = 0; j <= 6; j++) {
+        html += '<td class="calendar-day">';
+        if (day <= monthLength && (i > 0 || j >= startingDay)) {
+          html += day;
+          day++;
+        }
+        console.log(day);
+        html += '</td>';
+      }
+      // stop making rows if we've run out of days
+      if (day > monthLength) {
+        break;
+      } else {
+        html += '</tr><tr>';
+      }
+    }
+    html += '</tr></table>';
+
+    this.html = html;
+}
+
+Calendar.prototype.getHTML = function() {
+  return this.html;
+}
+
+
+const calendar = new Calendar(null, null, null, calEvents);
+calendar.generateHTML();
+document.write(calendar.getHTML());
